a6f23b2ab895e1cbee7064e68b2d5fdecfb6c65a
GURTAJ SINGH <gurtajsinghhhh1@gmail.com>
2026-04-15 14:27:54 +0530

    Create pg.cmd

---
+++ pg.cmd
@@ -0,0 +1,14 @@
+📋 3. Essential psql Meta-Commands
+
+These commands are used inside the psql shell (they start with \):
+
+Command	Description
+\l	List all databases
+\c dbname	Connect to a database
+\dt	List tables
+\d tablename	Describe a table
+\du	List users/roles
+\dn	List schemas
+\df	List functions
+\conninfo	Show current connection
+\q	Quit
